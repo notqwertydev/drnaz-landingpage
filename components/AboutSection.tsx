4a569f8667bd11adf9f292f43b7be3cf6ad8f195
Michel Fawaz <Michelfawaz87@gmail.com>
2024-01-25 00:49:53 +0200

    .

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -7,15 +7,24 @@ const AboutSection = () => {
         <section id='about' className="lg:p-8 overflow-hidden">
             <div className="grid grid-cols-1 md:grid-cols-2  gap-6">
                 <section className="py-3">
+                    <h1 className="font-normal tracking-tighter uppercase text-6xl lg:text-[8rem] pb-2">
+
+                        <span className="text-white">ABOUT </span>
+                        
+
+
+
+
+                    </h1>
                     <div className="rounded-3xl overflow-hidden bg-blackr h-full">
                         <div className="overflow-hidden h-full">
-                            <div className="p-10 grid content-end">
+                            <div className="p-2 grid content-end">
                                 <div>
                                     <div className="flex items-center">
                                     </div>
-                                    <p className="mt-12 text-xl font-medium tracking-tighter text-white font-display lg:text-2xl">
+                                    {/* <p className="mt-12 text-xl font-medium tracking-tighter text-white font-display lg:text-2xl">
                                         About
-                                    </p>
+                                    </p> */}
                                     <p className="mt-12 text-xl font-medium tracking-tighter text-white font-display lg:text-2xl">
                                         Dr. Naz is a full time dental surgeon with a passion for music. Dj-ing started as a hobby, that then developed into a more regular venture with requests piling up.
                                         Fast forward to today, where he has converted his hobby into a full blown career, playing alongside some of the biggest names in the industry and in clubs all over the world.
