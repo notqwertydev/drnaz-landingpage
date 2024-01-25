3a7fea5e2820a4c13e02f9ab66cc2c51d5575a29
Michel Fawaz <Michelfawaz87@gmail.com>
2024-01-25 22:12:34 +0200

    .

--- components/ContactSection.tsx
+++ components/ContactSection.tsx
@@ -301,7 +301,7 @@ const ContactSection = () => {
                                                                 selected={field.value}
                                                                 onSelect={field.onChange}
                                                                 disabled={(date) =>
-                                                                    date > new Date() || date < new Date("1900-01-01")
+                                                                    date < new Date("1900-01-01")
                                                                 }
                                                                 initialFocus
                                                             />
